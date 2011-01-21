e03c2e4d705b7d39e68cef91db8b271ede461ad2
J. Weir <john@smokinggun.com>
2011-01-20 23:45:28 -0500

    Basic compiler in place

--- spec/compiler_spec.js
+++ spec/compiler_spec.js
@@ -1,5 +1,33 @@
-var parser = require(__dirname+"/../lib/parser.js").parser;
+var parser   = require(__dirname+"/../lib/parser.js").parser;
+var compiler = require(__dirname+"/../lib/compiler.js").compiler;
+var _        = require(__dirname+"/../lib/vendor/underscore");
 
-describe('simple', function(){
+function c(input){
+   var compiled = compiler(input);
+   return (new Function(compiled))();
+}
+
+describe('text', function(){
+  it("should return the quoted text", function(){
+    expect(compiler([["text","'hello' \"world\""]])).toEqual("'hello' \"world\"");
+  });
 });
 
+describe('value', function(){
+  it("should return the value", function(){
+    expect(compiler([["value","a"]], {a:"foo"})).toEqual("foo");
+  });
+});
+
+describe('if', function(){
+  it("should return if true", function(){
+    expect(compiler([["if","1 > 2", ["text", "true"]]])).toEqual("");
+    expect(compiler([["if","3 > 2", ["text", "true"]]])).toEqual("true");
+  });
+});
+
+describe('each', function(){
+  it("should repeat the block for each item", function(){
+    expect(compiler([["each","[1,2,3]", ["text", "x "]]])).toEqual("x x x ");
+  });
+});
